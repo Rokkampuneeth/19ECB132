aab232b4a1afd3f6bc4d28183cceafac53323556
Rokkampuneeth <101515786+Rokkampuneeth@users.noreply.github.com>
2022-05-15 22:14:02 +0530

    Add files via upload

---
+++ singular linked list.cpp
@@ -0,0 +1,17 @@
+class Node {
+{
+    this.value = value; 
+    this.next = null;
+  }
+}
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+  }
+  pop() {
+  }
+}
