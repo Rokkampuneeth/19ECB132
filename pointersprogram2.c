e4843c55dae7528823b12364460f87cabeed254a
Rokkam puneeth <101515786+Rokkampuneeth@users.noreply.github.com>
2022-05-31 11:47:09 +0000

    added output to the program

--- pointersprogram2.c
+++ pointersprogram2.c
@@ -25,3 +25,11 @@ printf(" The Alphabets are : \n");
     return(0);
 }
 
+
+Pointer : Print all the alphabets:
+----------------------------------------
+ The Alphabets are : 
+ A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z 
+
+
+
