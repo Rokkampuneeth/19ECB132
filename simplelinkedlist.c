068d1442399ba50bb8179d69d6797913f231f205
Rokkam puneeth <101515786+Rokkampuneeth@users.noreply.github.com>
2022-05-31 11:55:13 +0000

    added output for the program

--- simplelinkedlist.c
+++ simplelinkedlist.c
@@ -63,3 +63,19 @@ void display() {
   }
   printf("\nNo Of Items In Linked List : %d", count);
 }
+
+
+singly(Single) Linked List Example - Using Functions
+
+Enter Element for Insert Linked List (-1 to Exit ) : 
+3
+
+Enter Element for Insert Linked List (-1 to Exit ) : 
+4
+
+Enter Element for Insert Linked List (-1 to Exit ) : 
+-1
+
+Display Linked List : 
+# 3 # # 4 # 
+No Of Items In Linked List : 2
