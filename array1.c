bbd556e17264cbe3cecc32bf372c4649138d2732
Rokkam puneeth <101515786+Rokkampuneeth@users.noreply.github.com>
2022-05-31 11:21:17 +0000

    added output for the program

--- array1.c
+++ array1.c
@@ -20,3 +20,20 @@ void  main()
     } 
     printf("\n");	
 }
+
+
+Read and Print elements of an array:
+----------------------
+Input 10 elements in the array :
+element - 0 : 1
+element - 1 : 2
+element - 2 : 3
+element - 3 : 4
+element - 4 : 5
+element - 5 : 6
+element - 6 : 7
+element - 7 : 8
+element - 8 : 9
+element - 9 : 10
+
+Elements in array are: 1  2  3  4  5  6  7  8  9  10  
